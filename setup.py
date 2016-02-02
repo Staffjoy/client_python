d2a9890e7b22bfd5eb2e793f654cafbcd65fa6ca
Philip I. Thomas <philip@staffjoy.com>
2016-02-02 15:46:52 -0800

    clean up requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from distutils.core import setup
 
 setup(
     name = "staffjoy",
-    packages = ["staffjoy"], 
+    packages = ["staffjoy"],
     version = "0.1",
     description = "Staffjoy API Wrapper in Python",
     author = "Philip Thomas",
     author_email = "help@staffjoy.com",
     url = "https://github.com/staffjoy/client_python",
     download_url = "https://github.com/staffjoy/client_python/Tarball/0.1",  # TODO - how does tarball get generated?
-    keywords = ["staffjoy-api", "staffjoy", "staff joy"], 
+    keywords = ["staffjoy-api", "staffjoy", "staff joy"],
+    install_requires = [
+        "requests[security]"
+    ]
 )
