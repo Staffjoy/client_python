a7abe60a1101a000806797837bc53cf2a0c688cb
Philip I. Thomas <mail@philipithomas.com>
2016-02-21 15:20:47 -0800

    Release version 0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(name="staffjoy",
       packages=["staffjoy"],
-      version="0.3",
+      version="0.4",
       description="Staffjoy API Wrapper in Python",
       author="Philip Thomas",
       author_email="help@staffjoy.com",
       license="MIT",
       url="https://github.com/staffjoy/client_python",
-      download_url="https://github.com/StaffJoy/client_python/archive/0.3.tar.gz",
+      download_url="https://github.com/StaffJoy/client_python/archive/0.4.tar.gz",
       keywords=["staffjoy-api", "staffjoy", "staff joy"],
       install_requires=["requests[security]"], )
