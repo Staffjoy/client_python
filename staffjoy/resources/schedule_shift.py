77a2d97fa3e3160a4cbe287f4f78463d581ce5ae
andhess <andrewclaytonhess@gmail.com>
2016-02-21 15:12:53 -0800

    updated test + pr feedback

--- staffjoy/resources/schedule_shift.py
+++ staffjoy/resources/schedule_shift.py
@@ -2,4 +2,5 @@ from ..resource import Resource
 
 
 class ScheduleShift(Resource):
+    """this is only a get collection endpoint"""
     PATH = "organizations/{organization_id}/locations/{location_id}/roles/{role_id}/schedules/{schedule_id}/shifts/"
