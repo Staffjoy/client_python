50dc76ce536bb3b2ecf5ecbdd9b9932fc9ab615b
Philip I. Thomas <philip@staffjoy.com>
2016-02-23 11:01:47 -0800

    Fix bug for ID_NAME in create

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
+version = "0.11"
 setup(name="staffjoy",
       packages=find_packages(),
-      version="0.10",
+      version=version,
       description="Staffjoy API Wrapper in Python",
       author="Philip Thomas",
       author_email="help@staffjoy.com",
       license="MIT",
       url="https://github.com/staffjoy/client_python",
-      download_url="https://github.com/StaffJoy/client_python/archive/0.10.tar.gz",
+      download_url="https://github.com/StaffJoy/client_python/archive/%s.tar.gz" % version,
       keywords=["staffjoy-api", "staffjoy", "staff joy"],
       install_requires=["requests[security]"], )
