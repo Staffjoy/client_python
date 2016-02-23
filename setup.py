62a48f82f3c583fbf77c449904259334a595587b
Philip I. Thomas <mail@philipithomas.com>
2016-02-22 20:03:11 -0800

    Update download url

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name="staffjoy",
       author_email="help@staffjoy.com",
       license="MIT",
       url="https://github.com/staffjoy/client_python",
-      download_url="https://github.com/StaffJoy/client_python/archive/0.5.tar.gz",
+      download_url="https://github.com/StaffJoy/client_python/archive/0.6.tar.gz",
       keywords=["staffjoy-api", "staffjoy", "staff joy"],
       install_requires=["requests[security]"], )
