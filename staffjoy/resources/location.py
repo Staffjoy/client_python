2da9e73a6e976c95b8e4964cd8b466462dd8c820
andhess <andrewclaytonhess@gmail.com>
2016-05-02 13:29:45 -0700

    adding some kwargs

--- staffjoy/resources/location.py
+++ staffjoy/resources/location.py
@@ -7,8 +7,8 @@ class Location(Resource):
     PATH = "organizations/{organization_id}/locations/{location_id}"
     ID_NAME = "location_id"
 
-    def get_roles(self):
-        return Role.get_all(parent=self)
+    def get_roles(self, **kwargs):
+        return Role.get_all(parent=self, **kwargs)
 
     def get_role(self, id):
         return Role.get(parent=self, id=id)
@@ -16,8 +16,8 @@ class Location(Resource):
     def create_role(self, **kwargs):
         return Role.create(parent=self, **kwargs)
 
-    def get_managers(self):
-        return Manager.get_all(parent=self)
+    def get_managers(self, **kwargs):
+        return Manager.get_all(parent=self, **kwargs)
 
     def get_manager(self, id):
         return Manager.get(parent=self, id=id)
